cd209ccd40152f0fa50260fd181d0f68127e5c3d
lockst3r <ushakovqq@gmail.com>
2021-01-13 16:45:10 +0200

    add

--- lesson5/task1/src/index.js
+++ lesson5/task1/src/index.js
@@ -1,5 +1,7 @@
-import fetchUser from '../profile/gateway.js';
-import { printProfile } from '../index.js';
+// file path: src/index.js
+
+import fetchUser from './profile/gateway.js';
+import { printProfile } from './profile/index.js';
 
 fetchUser('github')
     .then(userData => printProfile({
--- lesson5/task1/src/profile/index.js
+++ lesson5/task1/src/profile/index.js
@@ -1,3 +1,5 @@
+// file path: src/profile/index.js
+
 export const printProfile = profileData => {
   const { name, company } = profileData;
   console.log(`${name} from ${company}`);   
